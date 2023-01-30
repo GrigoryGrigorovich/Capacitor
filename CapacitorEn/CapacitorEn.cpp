d32f86afae24c1f8cd5347b01f7431367f246543
GrigoryGrigorovich <grigorygrigorovich11@gmail.com>
2023-01-30 21:24:24 +0300

    first commit

---
+++ CapacitorEn/CapacitorEn.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+
+	double s, c;
+
+	const double epsilon = 8.854187817e-12;
+
+	cout << "Enter the area of the covers: " << endl;
+	cin >> s;
+
+	c = epsilon * s;
+
+	cout << "The capacitor capacity is: " << c << endl;
+
+	system("pause");
+	return 0;
+}
